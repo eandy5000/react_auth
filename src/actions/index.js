59fe286bcdf0e2312b1ab0698b1c8da7429ba73f
Andy Eccleston <eandy5000@yahoo.com>
2017-01-24 20:00:33 -0600

    mid signin action creator

---
+++ src/actions/index.js
@@ -0,0 +1,13 @@
+
+export function signinUser({ email, password }) {
+    //send credentials to server
+
+    //if credentials are good 
+    //-update state to indicate user is authenticated
+    //-save Jwt token
+    //-redirect to feature
+
+    //if request is bad
+    //-show an error
+
+}
