383efc95c10238e16f506aed792271c694dcd384
Andy Eccleston <eandy5000@yahoo.com>
2017-01-25 21:19:31 -0600

    mid header signin signout link rendering

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,44 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Link } from 'react-router'
 
 class Header extends Component {
+    renderLinks() {
+        if(this.props.authorized) {
+        return (
+                <li>
+                    <Link to="/signout">Sign Out</Link>
+                </li>
+                )
+        } else {
+        return [
+                <li key={1}>
+                    <Link to="/signin">Sign In</Link>
+                </li>,
+                <li key={2}>
+                    <Link to="/signup">Sign Up</Link>
+                </li>
+                ]
+        }
+    }
+    
     render() {
+        console.log(this.props.authorized)
         return (
             <nav>
                 <ul>
-                    <li>
-                    Sign In
-                    </li>
+                    <li><Link to="/">Home</Link></li>
+                    { this.renderLinks() }
                 </ul>
             </nav>
         )
     }
 }
 
-export default Header
+function mapStateToProps(state){
+    return {
+        authorized: state.auth.authorized
+    }
+}
+
+export default connect(mapStateToProps)(Header)
