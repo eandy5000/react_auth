f7be5b40d51a5569d0ccac802af26b0bcfa2eca6
Andy Eccleston <eandy5000@yahoo.com>
2017-01-25 20:46:18 -0600

    finished sign in user action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,37 @@
+import axios from 'axios'
+import { browserHistory } from 'react-router'
+import { AUTH_USER,
+         AUTH_ERROR
+         } from './types'
+
+const ROOT_URL = 'http://localhost:3090'
 
 export function signinUser({ email, password }) {
+    return function(dispatch) {
+    
     //send credentials to server
-
+    axios.post(`${ROOT_URL}/signin`, { email, password })
+        .then(response => {
     //if credentials are good 
     //-update state to indicate user is authenticated
+    dispatch({type: AUTH_USER})
     //-save Jwt token
+    localStorage.setItem('token', response.data.token)
     //-redirect to feature
+    browserHistory.push('/feature')
+        })
+        .catch(() => {
+        //if request is bad
+        //-show an error
+        dispatch(authError('bad login credentials'))
+        })
 
-    //if request is bad
-    //-show an error
+    }
+}
 
+export function authError(error) {
+    return {
+        type: AUTH_ERROR,
+        payload: error
+    }
 }
--- src/components/auth/signin.js
+++ src/components/auth/signin.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react'
 import { reduxForm } from 'redux-form'
+import * as actions from '../../actions/index'
 
 class SignIn extends Component {
     handleFormSubmit({ email, password }) {
-        console.log(email, password)
+        this.props.signinUser({ email, password})
+    }
+
+    renderAlert() {
+        if(this.props.errorMessage) {
+            return (
+                <h4 style={{color:'red'}}>{this.props.errorMessage}</h4>
+            )
+        }
     }
 
     render() {
     const { handleSubmit, fields: { email, password }} = this.props
-
+        console.log(this.props.errorMessage)
         return (
             <form onSubmit={
                 handleSubmit(this.handleFormSubmit.bind(this))
@@ -21,13 +30,20 @@ class SignIn extends Component {
                 <label>Password:</label>
                 <input {...password}  />
             </fieldset>
+            { this.renderAlert() }
             <button action="submit" >Sign In</button>
         </form>
         )
     }
 }
 
+function mapStateToProps(state) {
+    return {
+        errorMessage: state.auth.error
+    }
+}
+
 export default reduxForm({
     form: 'signin',
     fields: ['email', 'password']
-})(SignIn)
+}, mapStateToProps, actions)(SignIn)
