d5a7c270d46dd250acd06bfe63c90a51395cd902
Andy Eccleston <eandy5000@yahoo.com>
2017-01-28 07:15:40 -0600

    client side validation working

--- src/components/auth/signup.js
+++ src/components/auth/signup.js
@@ -1,32 +1,61 @@
-import React, { Component } from 'react'
-import { reduxForm } from 'redux-form'
-import * as actions from '../../actions/index'
-import { connect } from 'react-redux'
+import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
+import * as actions from '../../actions';
 
-class SignUp extends Component {
+class Signup extends Component {
 
-    render() {
-        const { handleSubmit, email, password, passwordConfirm } = this.props
+  render() {
+    const { handleSubmit, fields: { email, password, passwordConfirm }} = this.props;
 
-        return <form>
-                <fieldset>
-                    <label>Email:</label><br/>
-                    <input {...email} />
-                </fieldset>
-                <fieldset>
-                    <label>Password:</label><br/>
-                    <input {...password} />
-                </fieldset>
-                <fieldset>
-                    <label>Confirm Password:</label><br/>
-                    <input {...passwordConfirm} />
-                </fieldset><br/>
-                <button action="submit">Sign Up</button>
-               </form>
-    }
+    return (
+      <form >
+        <fieldset >
+          <label>Email:</label><br/>
+          <input {...email} />  
+        </fieldset>
+{email.touched && email.error && <h5 style={{color: 'red'}} >{email.error}</h5> }     
+        <fieldset >
+          <label>Password:</label><br/>
+          <input  {...password} type="password" />    
+        </fieldset>
+{ password.touched && password.error && <h5 style={{color: 'red'}}>{password.error}</h5> }
+        <fieldset >
+          <label>Confirm Password:</label><br/>
+          <input {...passwordConfirm} type="password" />  
+        </fieldset>
+{ passwordConfirm.touched && passwordConfirm.error && <h5 style={{color: 'red'}}>{passwordConfirm.error}</h5> }    
+        <br/>
+        <button action="submit" >Sign up!</button>
+      </form>
+    );
+  }
 }
 
+function validate(formProps) {
+  const errors = {};
+  if(!formProps.email){
+      errors.email = "Please enter email"
+  }
+
+  if(!formProps.password) {
+      errors.password = "please enter your password"
+  }
+
+  if(!formProps.passwordConfirm) {
+      errors.passwordConfirm = " Please confirm your password"
+  }
+
+  if(formProps.password !== formProps.passwordConfirm) {
+      errors.password = "Passwords do not match"
+  }
+
+  return errors;
+}
+
+
+
 export default reduxForm({
-    form: 'signup',
-    fields: ['email', 'password', 'passwordConfirm']
-}, null, actions)(SignUp)
+  form: 'signup',
+  fields: ['email', 'password', 'passwordConfirm'],
+  validate
+}, null, actions)(Signup);
